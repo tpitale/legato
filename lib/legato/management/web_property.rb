0435c7ba293b8839e7464333222d58d7746a790d
Sunil <sunil.silumala@wwindia.com>
2013-10-01 15:47:38 +0530

    added attributes for web property

--- lib/legato/management/web_property.rb
+++ lib/legato/management/web_property.rb
@@ -11,7 +11,7 @@ module Legato
         "/accounts/#{account_id}/webproperties/#{id}"
       end
 
-      attr_accessor :id, :name, :website_url, :account_id, :user
+      attr_accessor :id, :name, :website_url, :account_id, :user, :attributes
 
       def initialize(attributes, user)
         self.user = user
