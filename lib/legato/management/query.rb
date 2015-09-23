60e5ca90ed17a939d73b8aeb41aa6160952081a3
Tony Pitale <tpitale@gmail.com>
2015-09-23 16:08:21 -0400

    Add quota user and user ip to User

--- lib/legato/management/query.rb
+++ lib/legato/management/query.rb
@@ -3,17 +3,22 @@ module Legato
     class Query
       attr_reader :instance_klass, :base_url
 
+      attr_accessor :quota_user, :user_ip
+
       def initialize(base_url, instance_klass)
         @base_url = base_url
         @instance_klass = instance_klass
       end
 
       def to_params
-        {}
+        {
+          'quotaUser' => quota_user,
+          'userIp' => user_ip
+        }.reject {|_,v| v.nil?}
       end
 
       def to_query_string
-        nil
+        to_params.map {|k,v| [k,v].join("=")}.join("&")
       end
     end
   end
--- spec/lib/legato/user_spec.rb
+++ spec/lib/legato/user_spec.rb
@@ -2,53 +2,74 @@ require 'spec_helper'
 
 describe Legato::User do
   context "an instance of Legato::User" do
-    let(:klass) {Class.new}
-    let(:query) {stub(:to_params => "params", :instance_klass => klass)}
+    let(:instance_klass) { Class.new }
+    let(:klass) { Class.new.tap {|k| k.extend(Legato::Model)} }
+    let(:query) { Legato::Query.new(klass) }
+    let(:access_token) { stub }
+    let(:user) { Legato::User.new(access_token) }
 
-    before :each do
-      @access_token = stub
-      @user = Legato::User.new(@access_token)
+    before(:each) do
+      query.stubs(:instance_klass).returns(instance_klass)
+    end
+
+    it 'assigns a quota user to the query' do
+      Legato::Request.stubs(:new).returns(stub(:response => 'response'))
+
+      user.quota_user = 'user12345678'
+      user.request(query)
+
+      expect(query.to_params['quotaUser']).to eql('user12345678')
+    end
+
+    it 'assigns a user ip to the query' do
+      Legato::Request.stubs(:new).returns(stub(:response => 'response'))
+
+      user.user_ip = '127.0.0.1'
+      user.request(query)
+
+      expect(query.to_params['userIp']).to eql('127.0.0.1')
     end
 
     it 'returns a response for a given query' do
-      @access_token.stubs(:get).returns('a response')
+      query.stubs(:to_params).returns('params')
+      access_token.stubs(:get).returns('a response')
       Legato::Response.stubs(:new)
       query.stubs(:base_url).returns("the_api_url")
 
-      @user.request(query)
+      user.request(query)
 
-      Legato::Response.should have_received(:new).with('a response', klass)
-      @access_token.should have_received(:get).with("the_api_url", :params => "params")
+      Legato::Response.should have_received(:new).with('a response', instance_klass)
+      access_token.should have_received(:get).with("the_api_url", :params => "params")
     end
 
     it 'has accounts' do
       Legato::Management::Account.stubs(:all)
-      @user.accounts
-      Legato::Management::Account.should have_received(:all).with(@user)
+      user.accounts
+      Legato::Management::Account.should have_received(:all).with(user)
     end
 
     it 'has web_properties' do
       Legato::Management::WebProperty.stubs(:all)
-      @user.web_properties
-      Legato::Management::WebProperty.should have_received(:all).with(@user)
+      user.web_properties
+      Legato::Management::WebProperty.should have_received(:all).with(user)
     end
 
     it 'has profiles' do
       Legato::Management::Profile.stubs(:all)
-      @user.profiles
-      Legato::Management::Profile.should have_received(:all).with(@user)
+      user.profiles
+      Legato::Management::Profile.should have_received(:all).with(user)
     end
 
     it 'has segments' do
       Legato::Management::Segment.stubs(:all)
-      @user.segments
-      Legato::Management::Segment.should have_received(:all).with(@user)
+      user.segments
+      Legato::Management::Segment.should have_received(:all).with(user)
     end
 
     it 'has goals' do
       Legato::Management::Goal.stubs(:all)
-      @user.goals
-      Legato::Management::Goal.should have_received(:all).with(@user)
+      user.goals
+      Legato::Management::Goal.should have_received(:all).with(user)
     end
   end
 end
